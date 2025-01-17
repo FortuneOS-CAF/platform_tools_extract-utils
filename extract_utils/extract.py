LineageOS/android_tools_extract-utils

extract_utils: implement simg2img in python
Change-Id: I15eaadc1da6c83779e6ec3cbf12d6a970dcfad17

extract_utils/extract.py
@@ -18,12 +18,12 @@ from typing import Callable, Generator, Iterable, List, Optional, Set, Union
 from zipfile import ZipFile
 
 from extract_utils.fixups import fixups_type, fixups_user_type
+from extract_utils.sparse_img import SPARSE_HEADER_MAGIC, unsparse_images
 from extract_utils.tools import (
     brotli_path,
     lpunpack_path,
     ota_extractor_path,
     sdat2img_path,
-    simg2img_path,
 )
 from extract_utils.utils import (
     Color,
@@ -259,7 +259,7 @@ def update_extract_partitions(
 
 
 def find_sparse_raw_image_paths(input_path: str):
-    magic = 0xED26FF3A.to_bytes(4, 'little')
+    magic = SPARSE_HEADER_MAGIC.to_bytes(4, 'little')
     return find_files(['super'], input_path, magic)
 
 
@@ -392,20 +392,12 @@ def extract_sparse_raw_imgs(file_paths: List[str], output_dir: str):
         partition_chunks = partition_chunks_map.setdefault(output_file_name, [])
         partition_chunks.append(file_path)
 
-    procs: parallel_input_cmds = []
     for output_file_name, partition_chunks in partition_chunks_map.items():
         output_file_path = path.join(output_dir, output_file_name)
 
         partition_chunks.sort(key=partition_chunk_index)
 
-        procs.append(
-            (
-                output_file_name,
-                [simg2img_path] + partition_chunks + [output_file_path],
-            )
-        )
-
-    process_cmds_in_parallel(procs, fatal=True)
+        unsparse_images(partition_chunks, output_file_path)
 
     return new_file_paths
 

extract_utils/sparse_img.py
@@ -1,0 +1,147 @@
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+import os
+from ctypes import Structure, c_uint16, c_uint32, sizeof
+from mmap import ACCESS_READ, MAP_PRIVATE, mmap
+from os import SEEK_CUR, SEEK_SET, path
+from typing import BinaryIO, List
+
+from extract_utils.utils import read_mmap_chunked, write_zero
+
+SPARSE_HEADER_MAGIC = 0xED26FF3A
+SPARSE_HEADER_MAJOR_VERSION = 1
+
+
+class SparseHeader(Structure):
+    _fields_ = [
+        # 0xed26ff3a
+        ('magic', c_uint32),
+        # (0x1) - reject images with higher major versions
+        ('major_version', c_uint16),
+        # (0x0) - allow images with higher minor versions
+        ('minor_version', c_uint16),
+        # 28 bytes for first revision of the file format
+        ('file_hdr_sz', c_uint16),
+        # 12 bytes for first revision of the file format
+        ('chunk_hdr_sz', c_uint16),
+        # block size in bytes, must be a multiple of 4 (4096)
+        ('blk_sz', c_uint32),
+        # total blocks in the non-sparse output image
+        ('total_blks', c_uint32),
+        # total chunks in the sparse input image
+        ('total_chunks', c_uint32),
+        # CRC32 checksum of the original data
+        ('image_checksum', c_uint32),
+    ]
+
+
+class SparseChunkHeader(Structure):
+    _fields_ = [
+        # 0xCAC1 -> raw; 0xCAC2 -> fill; 0xCAC3 -> don't care, 0xCAC4 -> CRC32
+        ('chunk_type', c_uint16),
+        # Reserved field (unused, typically 0)
+        ('reserved', c_uint16),
+        # Size of the chunk in blocks in the output image
+        ('chunk_sz', c_uint32),
+        # Total size of the chunk in bytes, including the header and data
+        ('total_sz', c_uint32),
+    ]
+
+
+class UnsparseError(Exception):
+    pass
+
+
+class ChunkType:
+    RAW = 0xCAC1
+    FILL = 0xCAC2
+    DONT_CARE = 0xCAC3
+    CRC32 = 0xCAC4
+
+
+def unsparse_image(f: BinaryIO, o: BinaryIO, first_chunk_dont_care: bool):
+    mm = mmap(f.fileno(), 0, access=ACCESS_READ | MAP_PRIVATE)
+
+    header = SparseHeader.from_buffer(mm)
+    offset = sizeof(header)
+
+    if header.magic != SPARSE_HEADER_MAGIC:
+        raise UnsparseError(
+            f'Invalid magic header {hex(header.magic)}, '
+            f'expected {hex(SPARSE_HEADER_MAGIC)}'
+        )
+
+    if header.major_version != SPARSE_HEADER_MAJOR_VERSION:
+        raise UnsparseError(
+            f'Unsupported major version {header.major_version}, '
+            f'expected {SPARSE_HEADER_MAJOR_VERSION}'
+        )
+
+    if header.file_hdr_sz != sizeof(SparseHeader):
+        raise UnsparseError(
+            f'Invalid file header size {header.file_hdr_sz}, '
+            f'expected {sizeof(SparseHeader)}',
+        )
+
+    if header.blk_sz % 4 != 0:
+        raise UnsparseError(
+            f'Invalid block size {header.blk_sz}, expected multiple of 4'
+        )
+
+    total_size = header.blk_sz * header.total_blks
+    o.truncate(total_size)
+    o.seek(0, SEEK_SET)
+
+    chunk_data_size = 0
+    for i in range(header.total_chunks):
+        offset += chunk_data_size
+        chunk_header = SparseChunkHeader.from_buffer(mm, offset)
+        offset += sizeof(chunk_header)
+
+        # First chunk needs to be a don't care chunk if this is not the
+        # first super.img sparse chunk because the output file
+        # needs to be seeked to the correct offset
+        if i == 0 and first_chunk_dont_care:
+            assert chunk_header.chunk_type == ChunkType.DONT_CARE
+
+        chunk_data_size = chunk_header.total_sz - header.chunk_hdr_sz
+        chunk_size_in_bytes = chunk_header.chunk_sz * header.blk_sz
+
+        if chunk_header.chunk_type == ChunkType.RAW:
+            assert chunk_data_size == chunk_size_in_bytes
+
+            for data_chunk in read_mmap_chunked(mm, chunk_data_size, offset):
+                o.write(data_chunk)
+        elif chunk_header.chunk_type == ChunkType.FILL:
+            fill_value = mm[offset : offset + chunk_data_size]
+
+            if fill_value == b'\x00\x00\x00\x00':
+                write_zero(o, chunk_size_in_bytes)
+            else:
+                for _ in range(chunk_size_in_bytes // chunk_data_size):
+                    o.write(fill_value)
+        elif chunk_header.chunk_type == ChunkType.DONT_CARE:
+            o.seek(chunk_size_in_bytes, SEEK_CUR)
+        elif chunk_header.chunk_type == ChunkType.CRC32:
+            pass
+        else:
+            raise UnsparseError(
+                f'Unknown chunk type {hex(chunk_header.chunk_type)}'
+            )
+
+
+def unsparse_images(input_paths: List[str], output_path: str):
+    if path.exists(output_path):
+        os.remove(output_path)
+
+    with open(output_path, 'wb') as o:
+        is_first = True
+        for input_path in input_paths:
+            with open(input_path, 'rb') as i:
+                unsparse_image(i, o, first_chunk_dont_care=not is_first)
+            is_first = False

extract_utils/utils.py
@@ -12,8 +12,10 @@ import os
 import shutil
 from enum import Enum
 from functools import cache
+from mmap import mmap
+from os import SEEK_CUR
 from subprocess import PIPE, Popen, run
-from typing import Generator, Iterable, List, Tuple
+from typing import BinaryIO, Generator, Iterable, List, Tuple
 
 
 def import_module(module_name, module_path):
@@ -207,3 +209,26 @@ def TemporaryWorkingDirectory(dir_path: str) -> Generator[None, None, None]:
         yield
     finally:
         os.chdir(cwd)
+
+
+def read_mmap_chunked(
+    mm: mmap,
+    size: int,
+    offset=0,
+    chunk_size=0x100000,
+):
+    while size > 0:
+        read_size = min(chunk_size, size)
+        data = mm[offset : offset + read_size]
+        offset += read_size
+
+        if not data:
+            raise ValueError('Size bigger than stream')
+
+        yield data
+        size -= len(data)
+
+
+def write_zero(f: BinaryIO, size: int):
+    f.seek(size - 1, SEEK_CUR)
+    f.write(b'\x00')

FortuneOS-CAF/platform_tools_extract-utils

extract_utils/extract.py
@@ -16,12 +16,12 @@ from typing import Callable, Dict, Iterable, List, Optional, Union
 from zipfile import ZipFile, is_zipfile
 
 from extract_utils.file import File
+from extract_utils.sparse_img import SPARSE_HEADER_MAGIC, unsparse_images
 from extract_utils.tools import (
     brotli_path,
     lpunpack_path,
     ota_extractor_path,
     sdat2img_path,
-    simg2img_path,
 )
 from extract_utils.utils import Color, color_print, run_cmd, scan_tree
 
@@ -196,7 +196,7 @@ def find_alternate_partitions(
 
 
 def find_sparse_raw_paths(partition: str, input_path: str):
-    magic = 0xED26FF3A.to_bytes(4, 'little')
+    magic = SPARSE_HEADER_MAGIC.to_bytes(4, 'little')
     return find_files(input_path, partition, magic=magic)
 
 
@@ -307,13 +307,7 @@ def extract_sparse_raw_img(file_paths: List[str], output_dir: str):
     file_paths.sort(key=partition_chunk_index)
     output_file_path = path.join(output_dir, output_file_name)
 
-    run_cmd(
-        [
-            simg2img_path,
-            *file_paths,
-            output_file_path,
-        ]
-    )
+    unsparse_images(file_paths, output_file_path)
 
 
 def unslot_partition(partition_slot: str):

extract_utils/sparse_img.py
@@ -1,0 +1,162 @@
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+import os
+from ctypes import Structure, c_uint16, c_uint32, sizeof
+from mmap import ACCESS_READ, MAP_PRIVATE, mmap
+from os import SEEK_CUR, SEEK_SET, path
+from typing import BinaryIO, List
+
+from extract_utils.utils import read_mmap_chunked, write_zero
+
+SPARSE_HEADER_MAGIC = 0xED26FF3A
+
+SPARSE_HEADER_MAJOR_VER = 1
+SPARSE_HEADER_MINOR_VER = 0
+
+
+class SparseHeader(Structure):
+    _fields_ = [
+        # 0xed26ff3a
+        ('magic', c_uint32),
+        # (0x1) - reject images with higher major versions
+        ('major_version', c_uint16),
+        # (0x0) - allow images with higher minor versions
+        ('minor_version', c_uint16),
+        # 28 bytes for first revision of the file format
+        ('file_hdr_sz', c_uint16),
+        # 12 bytes for first revision of the file format
+        ('chunk_hdr_sz', c_uint16),
+        # block size in bytes, must be a multiple of 4 (4096)
+        ('blk_sz', c_uint32),
+        # total blocks in the non-sparse output image
+        ('total_blks', c_uint32),
+        # total chunks in the sparse input image
+        ('total_chunks', c_uint32),
+        # CRC32 checksum of the original data
+        ('image_checksum', c_uint32),
+    ]
+
+
+class SparseChunkHeader(Structure):
+    _fields_ = [
+        # 0xCAC1 -> raw; 0xCAC2 -> fill; 0xCAC3 -> don't care, 0xCAC4 -> CRC32
+        ('chunk_type', c_uint16),
+        # Reserved field (unused, typically 0)
+        ('reserved', c_uint16),
+        # Size of the chunk in blocks in the output image
+        ('chunk_sz', c_uint32),
+        # Total size of the chunk in bytes, including the header and data
+        ('total_sz', c_uint32),
+    ]
+
+
+class UnsparseError(Exception):
+    pass
+
+
+class ChunkType:
+    RAW = 0xCAC1
+    FILL = 0xCAC2
+    DONT_CARE = 0xCAC3
+    CRC32 = 0xCAC4
+
+
+def unsparse_image(f: BinaryIO, o: BinaryIO, first_chunk_dont_care: bool):
+    mm = mmap(f.fileno(), 0, access=ACCESS_READ | MAP_PRIVATE)
+
+    header = SparseHeader.from_buffer(mm)
+
+    if header.magic != SPARSE_HEADER_MAGIC:
+        raise UnsparseError(
+            f'Invalid magic header {hex(header.magic)}, '
+            f'expected {hex(SPARSE_HEADER_MAGIC)}'
+        )
+
+    if header.major_version != SPARSE_HEADER_MAJOR_VER:
+        raise UnsparseError(
+            f'Unsupported major version {header.major_version}, '
+            f'expected {SPARSE_HEADER_MAJOR_VER}'
+        )
+
+    if header.minor_version != SPARSE_HEADER_MINOR_VER:
+        raise UnsparseError(
+            f'Unsupported minor version {header.minor_version}, '
+            f'expected {SPARSE_HEADER_MINOR_VER}'
+        )
+
+    if header.file_hdr_sz < sizeof(SparseHeader):
+        raise UnsparseError(
+            f'Invalid file header size {header.file_hdr_sz}, '
+            f'expected {sizeof(SparseHeader)}',
+        )
+
+    if header.chunk_hdr_sz < sizeof(SparseChunkHeader):
+        raise UnsparseError(
+            f'Invalid chunk header size {header.chunk_hdr_sz}, '
+            f'expected {sizeof(SparseChunkHeader)}',
+        )
+
+    if not header.blk_sz or header.blk_sz % 4 != 0:
+        raise UnsparseError(
+            f'Invalid block size {header.blk_sz}, expected multiple of 4'
+        )
+
+    total_size = header.blk_sz * header.total_blks
+    o.truncate(total_size)
+    o.seek(0, SEEK_SET)
+    offset = header.file_hdr_sz
+
+    chunk_data_size = 0
+    for i in range(header.total_chunks):
+        offset += chunk_data_size
+        chunk_header = SparseChunkHeader.from_buffer(mm, offset)
+        offset += header.chunk_hdr_sz
+
+        # First chunk needs to be a don't care chunk if this is not the
+        # first super.img sparse chunk because the output file
+        # needs to be seeked to the correct offset
+        if i == 0 and first_chunk_dont_care:
+            assert chunk_header.chunk_type == ChunkType.DONT_CARE
+
+        chunk_data_size = chunk_header.total_sz - header.chunk_hdr_sz
+        chunk_size_in_bytes = chunk_header.chunk_sz * header.blk_sz
+
+        if chunk_header.chunk_type == ChunkType.RAW:
+            assert chunk_data_size == chunk_size_in_bytes
+
+            for data_chunk in read_mmap_chunked(mm, chunk_data_size, offset):
+                o.write(data_chunk)
+        elif chunk_header.chunk_type == ChunkType.FILL:
+            assert chunk_data_size == 4
+            fill_value = mm[offset : offset + chunk_data_size]
+
+            if fill_value == b'\x00\x00\x00\x00':
+                write_zero(o, chunk_size_in_bytes)
+            else:
+                for _ in range(chunk_size_in_bytes // chunk_data_size):
+                    o.write(fill_value)
+        elif chunk_header.chunk_type == ChunkType.DONT_CARE:
+            o.seek(chunk_size_in_bytes, SEEK_CUR)
+        elif chunk_header.chunk_type == ChunkType.CRC32:
+            pass
+        else:
+            raise UnsparseError(
+                f'Unknown chunk type {hex(chunk_header.chunk_type)}'
+            )
+
+
+def unsparse_images(input_paths: List[str], output_path: str):
+    if path.exists(output_path):
+        os.remove(output_path)
+
+    with open(output_path, 'wb') as o:
+        is_first = True
+        for input_path in input_paths:
+            with open(input_path, 'rb') as i:
+                unsparse_image(i, o, first_chunk_dont_care=not is_first)
+            is_first = False

extract_utils/utils.py
@@ -12,9 +12,11 @@ import shutil
 from contextlib import contextmanager
 from enum import Enum
 from functools import lru_cache
+from io import SEEK_CUR
+from mmap import mmap
 from os import path
 from subprocess import PIPE, run
-from typing import Callable, Generator, Iterable, List, Optional
+from typing import BinaryIO, Callable, Generator, Iterable, List, Optional
 from urllib.request import Request, urlopen
 
 CHUNK_SIZE = 1024 * 1024
@@ -279,3 +281,26 @@ def urlretrieve_resume(
 
     if downloaded_size == 0:
         raise ValueError(f'Invalid file hash, expected {expected_sha256}')
+
+
+def read_mmap_chunked(
+    mm: mmap,
+    size: int,
+    offset=0,
+    chunk_size=0x100000,
+):
+    while size > 0:
+        read_size = min(chunk_size, size)
+        data = mm[offset : offset + read_size]
+        offset += read_size
+
+        if not data:
+            raise ValueError('Size bigger than stream')
+
+        yield data
+        size -= len(data)
+
+
+def write_zero(f: BinaryIO, size: int):
+    f.seek(size - 1, SEEK_CUR)
+    f.write(b'\x00')